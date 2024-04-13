6c879f2a41348164a0ae7189a939aeb6441c3dbd
karthik1220 <karthikgithub1102@gmail.com>
2024-04-13 16:01:08 +0530

    Initial commit

---
+++ Interface/src/main/java/com/kartheek/functional/FunctionalInterfaceApplication.java
@@ -0,0 +1,11 @@
+package com.kartheek.functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalInterfaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalInterfaceApplication.class, args);
+    }
+}
